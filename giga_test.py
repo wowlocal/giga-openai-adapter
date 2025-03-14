7d8d46801193306320674ad40fe4bd1cf3efe80e
Misha Kh <kh.misha.9@icloud.com>
2025-03-14 19:10:06 +0300

    giga test

--- giga_test.py
+++ giga_test.py
@@ -3,20 +3,80 @@ import certifi
 import ssl
 import os
 from dotenv import load_dotenv
+
+from gigachat.models import Chat, Function, FunctionParameters, Messages, MessagesRole, FunctionCall
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # load env variables
 load_dotenv()
 
-key = os.getenv("MASTER_TOKEN")
-
 # Path to your additional trusted certificate (.cer file)
 custom_cert_path = os.path.join(current_dir, "russian_trusted_root_ca.cer")
 
 # Укажите ключ авторизации, полученный в личном кабинете, в интерфейсе проекта GigaChat API
-with GigaChat(
-    credentials=key,
-    verify_ssl_certs=False  # Disable SSL verification
-) as giga:
-    response = giga.chat("Какие факторы влияют на стоимость страховки на дом?")
-    print(response.choices[0].message.content)
+import asyncio
+
+PAYLOAD = Chat(
+    messages=[
+        Messages(
+            role=MessagesRole.SYSTEM,
+            content="Ты - умный ИИ ассистент, который всегда готов помочь пользователю.",
+        ),
+        Messages(
+            role=MessagesRole.ASSISTANT,
+            content="Как я могу помочь вам?",
+        ),
+        Messages(
+            role=MessagesRole.USER,
+            content="Напиши подробный доклад на тему жизни Пушкина в Москве",
+        ),
+    ],
+    update_interval=0.1,
+)
+
+search_fn = Function(
+        name="duckduckgo_search",
+        description="""Поиск в DuckDuckGo.
+Полезен, когда нужно ответить на вопросы о текущих событиях.
+Входными данными должен быть поисковый запрос.""",
+        parameters=FunctionParameters(
+            type="object",
+            properties={"query": {"type": "string", "description": "Поисковый запрос"}},
+            required=["query"],
+        ),
+    )
+
+async def main():
+    key = os.getenv("MASTER_TOKEN")
+    async with GigaChat(
+        credentials=key,
+        verify_ssl_certs=False
+    ) as giga:
+        print(giga.get_models())
+        response = giga.embeddings(["Hello world!"])
+        # print(response)
+
+    async with GigaChat(
+        credentials=key,
+        verify_ssl_certs=False  # Disable SSL verification
+    ) as giga:
+        payload = "Найди что-нибудь интересное в интернете"
+        chat = Chat(
+            messages=[
+                Messages(
+                    role=MessagesRole.USER,
+                    content=payload,
+                ),
+            ],
+            functions=[search_fn],
+        )
+        async for chunk in giga.astream(chat):
+            if hasattr(chunk, 'choices') and chunk.choices:
+                for choice in chunk.choices:
+                    if choice.delta and choice.delta.content:
+                        print(choice.delta.content, end="", flush=True)
+
+# Run the async function
+if __name__ == "__main__":
+    asyncio.run(main())
